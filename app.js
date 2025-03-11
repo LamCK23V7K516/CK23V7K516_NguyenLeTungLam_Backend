5f18f598f9cd2f5709e92f75167be08ee4840397
LamCK23V7K516 <lamck23v7k516@vlvh.ctu.edu.vn>
2025-03-11 16:35:16 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
-const contactsRouter = require("./app/routes/contact.route");
 
 app.use(cors());
 app.use(express.json());
@@ -12,5 +13,17 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/contacts", contactsRouter);
+
+// handle 404 response
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+// define error-handling middleware
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
 
 module.exports = app;
